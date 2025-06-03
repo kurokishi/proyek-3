2be70cd208509c4488fbd5a7ef82d8e8e7299b53
kurokishi <kuroreidho@gmail.com>
2025-06-03 11:25:18 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+import streamlit as st
+
+def get_tickers():
+    tickers_input = st.sidebar.text_input("Masukkan kode saham (pisahkan dengan koma)", value="UNVR.JK, BBCA.JK")
+    return [t.strip().upper() for t in tickers_input.split(",") if t.strip()]
+
+def format_rupiah(x):
+    try:
+        return f"Rp{round(x):,}".replace(",", ".")
+    except:
+        return "Rp0"
